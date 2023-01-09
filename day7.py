70ae50a31744a7781359d653f384e04ad8b93509
DaisyVinter <daisy.vinter@postgrad.manchester.ac.uk>
2023-01-09 14:57:22 +0000

    fixed problem with input reading by changing strip to slicing

--- day7.py
+++ day7.py
@@ -13,35 +13,34 @@ folder_dict = {}
 with open(input_file) as f:
     for line in f:
         if line.startswith('$ cd '):
-            folder = line.strip('\n').removeprefix('$ cd ')
-            print (folder)
+            folder = line.strip('\n')[5:]
             if folder != '..':
                 folder_dict[folder] = []
         elif line.startswith('dir'):
-            folder_dict[folder].append(line.strip('dir ').strip('\n'))
+            folder_dict[folder].append(line.strip('\n')[4:])
         elif line[0] in string.digits:
             folder_dict[folder].append(line.strip('\n').rstrip(string.ascii_letters + string.punctuation).rstrip())
             
 
 sizes = {}
 
-#for key in folder_dict.keys():
-    # folder = folder_dict[key]
-    # sizes[key] = 0
+for key in folder_dict.keys():
+    folder = folder_dict[key]
+    sizes[key] = 0
 
     
-# folder = folder_dict['/']
-# sizes['/'] = 0
+folder = folder_dict['qsmg']
+sizes['/'] = 0
 
-# def sum_files(folder, folder_dict):
-#     size = 0
-#     for i in folder:
-#         if i.isnumeric():
-#             print(i)
-#             size += int(i)
-#         else:
-#             sum_files(folder_dict[i], folder_dict)
-#     return size
+def sum_files(folder, folder_dict):
+    size = 0
+    for i in folder:
+        if i.isnumeric():
+            print(i)
+            size += int(i)
+        else:
+            sum_files(folder_dict[i], folder_dict)
+    return size
 
-# size = sum_files(folder, folder_dict)
+size = sum_files(folder, folder_dict)
 
